200dcad08748158691434683b30fa9752fa51b5e
Kyle Wenholz <kyle@krwenholz.com>
2025-12-20 01:16:54 +0000

    Bad Day 2 Part 1

--- src/days/day02.ts
+++ src/days/day02.ts
@@ -1,12 +1,44 @@
 import { Day } from "../day";
 
+type ID = {
+  len: number;
+  val: number;
+};
+
+type Range = {
+  start: ID;
+  end: ID;
+};
+
+function isSymmetric(id: ID): bool {
+  return false;
+}
+
 export class Day02 extends Day {
   day = 2;
   name = "TODO: Add puzzle name";
 
   partOne(input: string): string | number {
-    const _lines = input.trim().split("\n");
-    // TODO: Implement part one
+    const rangeStrs = input.trim().split(",");
+    const ranges: Range[] = [];
+    for (const r of rangeStrs) {
+      const rMatch = r.match(/(\d+)-(\d+)/);
+      if (rMatch) {
+        var [_, start, end] = rMatch;
+        start = start || "";
+        end = end || "";
+        ranges.push({
+          start: { len: start.length, val: Number(start) },
+          end: { len: end.length, val: Number(end) },
+        });
+      } else {
+        throw new Error(`Unmatched ${r}`);
+      }
+    }
+
+    const filtered = ranges.filter((r) => isSymmetric(r.start) && isSymmetric(r.end));
+
+    ranges.map((r) => console.log(r));
     return "Not implemented";
   }
 
